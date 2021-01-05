40d160ab4579d83a593071680aa7594eb42722bf
Adefolarin Osu <folzieds@gmail.com>
2021-01-05 11:06:05 +0100

    initial commit

---
+++ src/main/java/com/phos/cron/config/DataSourceConfig.java
@@ -0,0 +1,10 @@
+package com.phos.cron.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Folarin on 05/01/2021
+ */
+@Configuration
+public class DataSourceConfig {
+}
