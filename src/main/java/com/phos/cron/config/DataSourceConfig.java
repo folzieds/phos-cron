b9944978ddc5c39b6217615f97e963c5f8bbba60
Adefolarin Osu <folzieds@gmail.com>
2021-01-05 11:34:32 +0100

    Added datasource configuration

--- src/main/java/com/phos/cron/config/DataSourceConfig.java
+++ src/main/java/com/phos/cron/config/DataSourceConfig.java
@@ -1,10 +1,39 @@
 package com.phos.cron.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Folarin on 05/01/2021
  */
 @Configuration
 public class DataSourceConfig {
+
+    @Value("${datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${phos.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${phos.datasource.username}")
+    private String dataSourceUsername;
+
+    @Value("${phos.datasource.password}")
+    private String dataSourcePassword;
+
+    @Bean
+    public DataSource phosDataSource(){
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUsername);
+        dataSource.setPassword(dataSourcePassword);
+
+        return dataSource;
+    }
 }
